bd9e29b2e12042472b4b89ae34681b16ac47d261
EDA <eda@mac-mini-3.local>
2016-06-07 11:49:22 +1200

    Updates

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ xhr.get('https://api.wheretheiss.at/v1/satellites', function (err, data) {
   }
 
   console.log(data.body) // in case you're curious
+  console.log(data.body[0]) // in case you're curious
+
   var viewModel = {
     stations: data.body
   }
