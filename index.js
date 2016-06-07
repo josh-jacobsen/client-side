c356c7dcd1730fcdedf3d8fe1a8e7512e6a2bafc
EDA <eda@mac-mini-3.local>
2016-06-07 14:44:53 +1200

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function (err, data) {
+xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function (err, data) {
   if (err) {
     console.log(err)
   }
